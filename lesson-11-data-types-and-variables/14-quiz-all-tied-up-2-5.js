2532ef3995cf2e8de32777d63e4e5ccb7920b3bd
jameswos <36367450+jameswos@users.noreply.github.com>
2018-02-11 16:40:41 +0000

    Update 14-quiz-all-tied-up-2-5.js

--- lesson-11-data-types-and-variables/14-quiz-all-tied-up-2-5.js
+++ lesson-11-data-types-and-variables/14-quiz-all-tied-up-2-5.js
@@ -1,6 +1,12 @@
+
 /*
  * Programming Quiz: All Tied Up (2-5)
+ * Build a single string that resembles the following joke.
+ *
+ * Why couldn't the shoes go out and play?
+ * They were all "tied" up!
+ *
+ * Your joke should take the format of a question and answer. The first line should be a question and the second line should be an answer.
  */
-
 var joke = "What did the chicken yell? \n\"Hey, where\'s my feathers?\""
 console.log(joke);
