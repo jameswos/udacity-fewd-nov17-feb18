39e7e966feeed204f14a8018c48f8a037a67b9b2
jameswos <36367450+jameswos@users.noreply.github.com>
2018-02-11 16:42:36 +0000

    Create 21-quiz-whats-my-name-2-9.js

---
+++ lesson-11-data-types-and-variables/21-quiz-whats-my-name-2-9.js
@@ -0,0 +1,9 @@
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ *
+ * Create a variable called fullName and assign it your full name as a string.
+ */
+ 
+ // your code goes here
+var fullName = "James Woszczynski";
+console.log(fullName);
