4b5eb9e76d2ed552eee86d559c9db140ca93afcd
jameswos <36367450+jameswos@users.noreply.github.com>
2018-02-11 16:40:18 +0000

    Update 15-quiz-yosa-buson-2-6.js

--- lesson-11-data-types-and-variables/15-quiz-yosa-buson-2-6.js
+++ lesson-11-data-types-and-variables/15-quiz-yosa-buson-2-6.js
@@ -1,5 +1,14 @@
 /*
  * Programming Quiz: Yosa Buson (2-6)
+ *
+ * Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+ *
+ * Blowing from the west
+ * Fallen leaves gather
+ * In the east.
+ * Each string should be printed on its own line. Here is a link to our lesson on Escaped Characters as a reference.
+ *
+ * Hint: You will need to use special characters to produce the following output.
  */
 
 var haiku = "Blowing from the west" + "\nFallen leaves gather" + "\nIn the east."
